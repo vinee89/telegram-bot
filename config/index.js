151445d91adcb834b928eae34f67cc4d81d48b90
ivb98 <ivanvalette@hotmail.com>
2020-03-11 08:27:39 -0400

    added hotfix to database url

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 const production = process.env.NODE_ENV === "production" ? true : false;
 module.exports = {
     production,
-    connection_string: !production ? process.env.PROD_DB : process.env.DEV_DB,
+    connection_string: production ? process.env.PROD_DB : process.env.DEV_DB,
     tg_key: production ? process.env.PROD_TG_KEY : process.env.DEV_TG_KEY
 };
